61f7355b61aaef5f3c5d28e5346b6e187305d7c8
Dest <morm934@gmail.com>
2022-03-21 04:35:17 +0300

    module4-task1

--- js/main.js
+++ js/main.js
@@ -1,53 +1,8 @@
 const SIMILAR_POST_COUNT = 25;
-const SIMILAR_COMMENT_COUNT = 200;
 const SIMILAR_PHOTO_COUNT = 25;
-const SIMILAR_LIKES_COUNT = [15, 200];
-const [likesMin, likesMax] = SIMILAR_LIKES_COUNT;
-const ID_POST_ARRAY = Array.from({ length: SIMILAR_POST_COUNT }, (_item, index) => index + 1);
-const ID_COMMENT_ARRAY = Array.from({ length: SIMILAR_COMMENT_COUNT }, (_item, index) => index + 1);
-const PHOTO_ARRAY = Array.from({ length: SIMILAR_PHOTO_COUNT }, (_item, index) => index + 1);
+const LIKES_COUNT_MAX = 200;
+const LIKES_COUNT_MIN = 15;
 const MAX_PHOTO_COMMENTS = 4;
-
-const getPostID = getRandomUniqElement(ID_POST_ARRAY);
-const getCommentID = getRandomUniqElement(ID_COMMENT_ARRAY);
-const getPhoto = getRandomUniqElement(PHOTO_ARRAY);
-
-function getIntRandomNumber(min, max) {
-  let absMin = Math.abs(min);
-  let absMax = Math.abs(max);
-
-  if (absMin > absMax) {
-    [absMax, absMin] = [absMin, absMax];
-  }
-
-  return Math.floor(Math.random() * (absMax - absMin +  1)) + absMin;
-}
-
-function checkStringLength(string, maxLength) {
-  return string.length <= maxLength;
-}
-
-checkStringLength('строка', 4);
-
-getIntRandomNumber(-12,33);
-
-function getRandomArrayElement(arr) {
-  return arr[getIntRandomNumber(0, arr.length - 1)];
-}
-
-function getRandomIndex(arr) {
-  return Math.floor(Math.random() * arr.length);
-}
-
-function getRandomUniqElement(Array) {
-  let arr = Array.slice();
-  return () => {
-    if (arr.length === 0) {
-      arr = Array.slice();
-    }
-    return arr.splice(getRandomIndex(arr), 1)[0];
-  };
-}
 
 const SIMILAR_DESCRIPTIONS = [
   'Замечательное фото',
@@ -75,21 +30,60 @@ const SIMILAR_NAMES = [
   'Гость',
 ];
 
-const createComment = () => ({
-  id: getCommentID(),
+const PHOTO_ARRAY = Array.from({ length: SIMILAR_PHOTO_COUNT }, (_item, index) => index + 1);
+
+const getPhoto = getRandomUniqElement(PHOTO_ARRAY);
+
+function getIntRandomNumber(min, max) {
+  let absMin = Math.abs(min);
+  let absMax = Math.abs(max);
+
+  if (absMin > absMax) {
+    [absMax, absMin] = [absMin, absMax];
+  }
+
+  return Math.floor(Math.random() * (absMax - absMin +  1)) + absMin;
+}
+
+function checkStringLength(string, maxLength) {
+  return string.length <= maxLength;
+}
+
+checkStringLength('строка', 4);
+
+getIntRandomNumber(-12,33);
+
+function getRandomArrayElement(arr) {
+  return arr[getIntRandomNumber(0, arr.length - 1)];
+}
+
+function getRandomUniqElement(array) {
+  let arr = array.slice();
+
+  return () => {
+    if (!arr.length) {
+      arr = array.slice();
+    }
+
+    return arr.splice(getIntRandomNumber(0, arr.length - 1), 1)[0];
+  };
+}
+
+const createComment = (id) => ({
+  id: id,
   avatar: `img/avatar-${  getIntRandomNumber(1, 6)  }.svg`,
   message: getRandomArrayElement(SIMILAR_MESSAGES),
   name: getRandomArrayElement(SIMILAR_NAMES),
 });
 
-const createPost = () => ({
-  id: getPostID(),
+const createPost = (id) => ({
+  id: id,
   url: `photos/${  getPhoto()  }.jpg`,
   description: getRandomArrayElement(SIMILAR_DESCRIPTIONS),
-  likes: getIntRandomNumber(likesMin, likesMax),
-  comments: Array.from({length: getIntRandomNumber(1, MAX_PHOTO_COMMENTS)}, createComment)
+  likes: getIntRandomNumber(LIKES_COUNT_MIN, LIKES_COUNT_MAX),
+  comments: Array.from({length: getIntRandomNumber(1, MAX_PHOTO_COMMENTS)}, (_item, index) =>  createComment(index))
 });
 
-const createSmilarPosts = () => Array.from({length: SIMILAR_POST_COUNT}, createPost);
+const createSmilarPosts = () => Array.from({length: SIMILAR_POST_COUNT}, (_item, index) => createPost(index));
 
-export {createSmilarPosts};
+console.log(createSmilarPosts());
